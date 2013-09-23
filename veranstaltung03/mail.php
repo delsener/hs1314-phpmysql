f7fdc573ae1f2c8d299c8a44305aa5b29d0a426d
Murch <murch@buchheim.(none)>
2013-09-23 16:01:14 +0200

    uebung mail

---
+++ veranstaltung03/mail.php
@@ -0,0 +1,20 @@
+<?php 
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+
+	// mail settings
+	$subject = "PHP/Mysql Kurs - Mail Test - David Elsener (elsendav)";
+	$message = "Dieses Mail wurde per PHP Script gesendet.";
+	$recipient = "elsendav@students.zhaw.ch";
+	$headers = "From: elsendav@students.zhaw.ch";
+	
+	// print mail settings
+	echo "<br>sending mail to $recipient with subject $subject and the following content: ";
+	echo "<p>$message</p>";
+	
+	// send mail
+	$result = mail($recipient, $subject, $message, $headers);
+	
+	// print result
+	echo "mail function returned: $result";
+?>
