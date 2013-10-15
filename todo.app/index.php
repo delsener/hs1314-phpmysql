3fbab4290f5c0f366184e706a0372b89ef7026fd
Murch <murch@buchheim.(none)>
2013-10-15 02:02:48 +0200

    todo app

---
+++ todo.app/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ZHAW Mysql/PHP Modul - Todo App">
+    <meta name="author" content="delsener">
+    <link rel="shortcut icon" href="bootstrap/assets/ico/favicon.png">
+
+    <title>ZHAW Mysql/PHP - Todo App</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this bootstrap template -->
+    <link href="css/todoapp.css" rel="stylesheet">
+    
+	<?php 
+		require 'php/todo.php';
+		
+		if (isset($_POST['form_create_submitted'])) {
+			if (validateForm() == true) {
+				saveTask();
+			}
+		}
+		
+		if (isset($_GET['checked']) && isset($_GET['update_task_id'])) {
+			updateTaskStatus();
+		}
+		
+		if (isset($_GET['delete_task_id'])) {
+			deleteTask();
+		}
+	?>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <span class="navbar-brand">ZHAW Mysql/PHP - Selbststudium</span>
+        </div>
+        <div class="navbar-collapse collapse navbar-right">
+            <span class="navbar-author">delsener</span>
+         </div>
+      </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Todo App</h1>
+        <p>A small todo app written in PHP using a MySql database to store the todos.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <h2>Todo</h2>
+          <?php printTasks(0) ?>
+        </div>
+        <div class="col-lg-5">
+          <h2>Done</h2>
+           <?php printTasks(1) ?>
+       </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-5">
+          
+          
+          <?php 
+          	$id = null;
+          	$title = null;
+          	$date = null;
+          	$description = null;
+          	if (isset($_GET['task_id'])) {
+          		$task = fetchTask($_GET['task_id']);
+          		$id = $task['ID'];
+          		$title = $task['TITLE'];
+          		$date = $task['DATE'];
+          		$description = $task['DESCRIPTION'];
+          		$status = $task['STATUS'];
+          		
+          		echo "<h2>Modify task $id</h2>";
+          	} else {
+          		echo "<h2>Create new task</h2>";
+          	}
+          ?>
+          
+          <form action="index.php?modify" method="POST">
+          	<input type="hidden" name="form_create_submitted" id="form_create_submitted" value="create" />
+          	<input type="hidden" name="task_id" id="task_id" value="<?php echo $id ?>" />
+          
+          	<label for="title">Title</label>
+          	<input type="text" maxlength="100" name="title" id="title" value="<?php echo $title ?>" placeholder="Some tasks title" required="required" />
+          	<br>
+          	<label for="date">Date</label>
+          	<input type="date" name="date" id="date" value="<?php echo $date ?>" placeholder="14.10.2013" required="required" />
+          	<br>
+          	<label for="description">Description</label>
+          	<textarea rows="5" cols="50" type="description" name="description" id="description" placeholder="Some details describing the task ..." required="required"><?php echo $description ?></textarea>
+          	
+          	<br>
+          	<input type="submit" class="btn btn-default" value="Save task" name="submit" id="submit" />
+          </form>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <small>ZHAW - delsener</small>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap/assets/js/jquery.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ todo.app/php/todo.php
@@ -0,0 +1,143 @@
+<?php
+
+# Error level
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+# DB settings
+define("DB_CONNECTION", "mysql:host=localhost;dbname=buchheim");
+define("DB_USER", "murch");
+define("DB_PASSWORD", "buchheim");
+
+# debug mode
+define("DEBUGGING", false);
+
+function openDBConnection() {
+	try {
+		$pdo =  new PDO(DB_CONNECTION, DB_USER, DB_PASSWORD);
+		if (DEBUGGING == true && $pdo != null) {
+			echo 'Connection to DB established ..';
+		}
+		return $pdo;
+	} catch(PDOException $e) {
+		die ('Could not open connection to '.DB_CONNECTION);
+	}
+}
+
+function validateForm() {
+	return isset($_POST['title']) && isset($_POST['date']) && isset($_POST['description']);
+}
+
+function updateTaskStatus() {
+	$id = $_GET['update_task_id'];
+	
+	$db = openDBConnection();
+	
+	$sql = "UPDATE TASK SET STATUS =:status WHERE ID=:id";
+	$pquery = $db->prepare($sql);
+	$pquery->execute(array(':status'=>1, ':id'=>$id));
+	
+	if (DEBUGGING == true) {
+		$pquery->debugDumpParams();
+		echo var_dump($db->errorinfo());
+	}
+}
+
+function deleteTask() {
+	$id = $_GET['delete_task_id'];
+
+	$db = openDBConnection();
+
+	$sql = "DELETE FROM TASK  WHERE ID=:id";
+	$pquery = $db->prepare($sql);
+	$pquery->execute(array(':id'=>$id));
+}
+
+function saveTask() {
+	$title = $_POST['title'];
+	$date = $_POST['date'];
+	$description = $_POST['description'];
+
+	$db = openDBConnection();
+
+	if (isset($_POST['task_id'])) {
+		$sql = "UPDATE TASK SET TITLE = :title, DATE = :date, DESCRIPTION = :description, STATUS = :status where ID = :id";
+		$pquery = $db->prepare($sql);
+		$pquery->execute(array(':title'=>$title, ':date'=>$date, ':description'=>$description, ':status'=>0, ':id'=>$_POST['task_id']));
+	} else {
+		$sql = "INSERT INTO TASK (TITLE, DATE, DESCRIPTION, STATUS) VALUES (:title, :date, :description, :status)";
+		$pquery = $db->prepare($sql);
+		$pquery->execute(array(':title'=>$title, ':date'=>$date, ':description'=>$description, ':status'=>0));
+	}
+
+	if (DEBUGGING == true) {
+		$pquery->debugDumpParams();
+		echo var_dump($db->errorinfo());
+	}
+}
+
+function fetchTask($id) {
+	$db = openDBConnection();
+	
+	$sql = "SELECT * FROM TASK WHERE ID = :id";
+	$pquery = $db->prepare($sql);
+	$pquery->execute(array(':id'=>$id));
+	
+	$result = $pquery->fetch(PDO::FETCH_ASSOC);
+	if (DEBUGGING == true) {
+		echo "task id $id";
+		$pquery->debugDumpParams();
+		echo var_dump($result);
+		echo var_dump($db->errorinfo());
+	}
+	return $result;
+}
+
+function printTasks($status) {
+	
+	$db = openDBConnection();
+	
+	$sql = "SELECT * FROM TASK WHERE STATUS = :status ORDER BY DATE";
+	$pquery = $db->prepare($sql);
+	$pquery->execute(array(':status'=>$status));
+	
+	$result = $pquery->fetchAll(PDO::FETCH_ASSOC);
+	if (DEBUGGING == true) {
+		$pquery->debugDumpParams();
+		echo var_dump($result);
+		echo var_dump($db->errorinfo());
+	}
+	
+	echo '<table id="tasklist">';
+	
+	$index = 0;
+	foreach ($result as $row) {
+		echo '<tr class="task_row'.($index % 2).'">';
+		
+		if ($status == 0) {
+			echo '<td width="50"><a class="set_checked_link" href="index.php?update_task_id='.$row['ID'].'&checked=true"><img src="rsc/done_icon.png" alt="Done" /></a></td>';
+		}
+		
+		echo '<td width="200px"><b>'.$row['TITLE'].'</b></td>';
+		echo '<td width="100px">'.$row['DATE'].'</td>';
+		
+		$colspan = 2;
+		if ($status == 0) {
+			echo '<td><a class="modify_link" href="index.php?task_id='.$row['ID'].'">Modify</a>&nbsp;|&nbsp;<a class="modify_link" href="index.php?delete_task_id='.$row['ID'].'">Delete</a></td>';
+			$colspan = 3;
+		}
+		
+		echo '</tr><tr class="task_row'.($index % 2).'">';
+		
+		if ($status == 0) {
+			echo '<td></td>';
+		}
+		
+		echo '<td colspan="'.$colspan.'">'.$row['DESCRIPTION'].'</td>';
+		echo '</tr>';
+		$index++;
+	}
+	echo '</table>';
+}
+
+?>
